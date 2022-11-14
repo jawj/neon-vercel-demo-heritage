f8956de8e9991ff423f51553890c1ea0ec59e10d
George MacKerron <george@mackerron.co.uk>
2022-11-14 12:29:51 +0000

    Added DB query

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,27 +1,45 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Client } from 'pg';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 interface Data {
   longitude: number;
   latitude: number;
+  nearestSites: any[];
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  function getCoordinate(name: 'latitude' | 'longitude') {
+  function getCoord(coord: 'latitude' | 'longitude') {
+    const sources: [any, string][] = [
+      [req.query, coord],  // try query string: ?longitude=-12.34&latitude=56.78
+      [req.headers, `x-vercel-ip-${coord}`],  // try Vercel geolocation headers
+      [{ latitude: '37.81', longitude: '-122.47' }, coord]  // fall back to San Francisco
+    ];
     let result = NaN;
-    const sources: [any, string][] = [[req.query, ''], [req.headers, 'x-vercel-ip-'], [{ latitude: '37.81', longitude: '-122.47' }, '']];
-    for (const [source, prefix] of sources) {
-      const param = source[prefix + name];
+    for (const [source, key] of sources) {
+      const param = source[key];
       result = typeof param === 'string' ? parseFloat(param) : Array.isArray(param) ? parseFloat(param[0]) : result;
       if (!isNaN(result)) break;
     }
     return result;
   }
-  const longitude = getCoordinate('longitude');
-  const latitude = getCoordinate('latitude');
+  const longitude = getCoord('longitude');
+  const latitude = getCoord('latitude');
 
-  res.status(200).json({ longitude, latitude })
+  const client = new Client(process.env.DATABASE_URL);
+  await client.connect();
+
+  const { rows } = await client.query(`
+    select 
+      id_no, name_en, category,
+      st_makepoint($1, $2) <-> location as distance
+    from whc_sites_2021
+    order by distance limit 10`,
+    [longitude, latitude]
+  );  // no cast needed: PostGIS casts geometry -> geography, never the reverse: https://gis.stackexchange.com/a/367374
+
+  res.status(200).json({ longitude, latitude, nearestSites: rows });
+  await client.end();
 }
