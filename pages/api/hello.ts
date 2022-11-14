21a1531da17de1305cec3646ce0dfe4586248f3e
George MacKerron <george@mackerron.co.uk>
2022-11-14 12:07:20 +0000

    Geolocation API endpoint

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+interface Data {
+  longitude: number;
+  latitude: number;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  function getCoordinate(name: 'latitude' | 'longitude') {
+    let result = NaN;
+    const sources: [any, string][] = [[req.query, ''], [req.headers, 'x-vercel-ip-'], [{ latitude: '37.81', longitude: '-122.47' }, '']];
+    for (const [source, prefix] of sources) {
+      const param = source[prefix + name];
+      result = typeof param === 'string' ? parseFloat(param) : Array.isArray(param) ? parseFloat(param[0]) : result;
+      if (!isNaN(result)) break;
+    }
+    return result;
+  }
+  const longitude = getCoordinate('longitude');
+  const latitude = getCoordinate('latitude');
+
+  res.status(200).json({ longitude, latitude })
 }
