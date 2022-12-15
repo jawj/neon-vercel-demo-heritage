6485e8e6cb42b191c6bccb33e2c67d887944d453
George MacKerron <george@mackerron.co.uk>
2022-12-15 15:10:26 +0000

    Refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,55 @@ import Image from 'next/image';
 import useSWR, { SWRConfig } from 'swr';
 import type { SitesData } from './api/sites';
 
-export default function Home() {
+function Nearest() {
   const { data, error } = useSWR<SitesData>('/api/sites?db=gm-wss');
 
-  const { data: gmwss_1_data } = useSWR<SitesData>(() => data && `/api/sites?db=gm-wss&x=1`);
+  return <>
+    <h1>Your nearest world heritage sites</h1>
+    {error ? <p className="error">Error: failed to load</p> :
+      !data ? <p className="loading"><Image src="/loading.svg" alt="Loading spinner" width="66" height="66" /><br />Loading &hellip;</p> :
+        <div className="sites"><ul>{
+          data.nearestSites.map(site =>
+            <li key={site.id_no}><a href={`https://whc.unesco.org/en/list/${site.id_no}/`}
+              style={{ backgroundImage: `url(https://whc.unesco.org/uploads/sites/gallery/google/site_${site.id_no}.jpg` }}>
+              <span className="name">{site.name_en}</span>
+              <span className="category">{site.category}</span>
+              <span className="distance">{Math.round(site.distance / 1000)} km</span>
+            </a></li>
+          )}
+        </ul></div>
+    }
+  </>;
+}
+
+function Timings() {
+  const { data: gmwss_1_data } = useSWR<SitesData>(() => `/api/sites?db=gm-wss&x=1`);
   const { data: gmwss_2_data } = useSWR<SitesData>(() => gmwss_1_data && `/api/sites?db=gm-wss&x=2`);
   const { data: gmwss_3_data } = useSWR<SitesData>(() => gmwss_2_data && `/api/sites?db=gm-wss&x=3`);
   const { data: gmwss_4_data } = useSWR<SitesData>(() => gmwss_3_data && `/api/sites?db=gm-wss&x=4`);
   const { data: gmwss_5_data } = useSWR<SitesData>(() => gmwss_4_data && `/api/sites?db=gm-wss&x=5`);
 
+  return <>
+    <h2>Timings</h2>
+    <ul>
+      <li>
+        Secure WebSocket (wss://) to co-located proxy and DB: {' '}
+        {gmwss_1_data?.duration ?? '...'} ms, {' '}
+        {gmwss_2_data?.duration ?? '...'} ms, {' '}
+        {gmwss_3_data?.duration ?? '...'} ms, {' '}
+        {gmwss_4_data?.duration ?? '...'} ms, {' '}
+        {gmwss_5_data?.duration ?? '...'} ms {' '}
+      </li>
+    </ul>
+  </>;
+}
+
+export default function Home() {
   return (
     <SWRConfig value={{
+      fetcher: (url: string) => fetch(url).then(res => res.json()),
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
-      fetcher: (url: string) => fetch(url).then(res => res.json()),
     }}>
       <div>
         <Head>
@@ -25,33 +60,9 @@ export default function Home() {
           <meta name="viewport" content="initial-scale=.55"></meta>
         </Head>
 
-        <h1>Your nearest world heritage sites</h1>
+        <Nearest />
+        <Timings />
 
-        {error ? <p className="error">Error: failed to load</p> :
-          !data ? <p className="loading"><Image src="/loading.svg" alt="Loading spinner" width="66" height="66" /><br />Loading &hellip;</p> :
-            <div className="sites"><ul>{
-              data.nearestSites.map(site =>
-                <li key={site.id_no}><a href={`https://whc.unesco.org/en/list/${site.id_no}/`}
-                  style={{ backgroundImage: `url(https://whc.unesco.org/uploads/sites/gallery/google/site_${site.id_no}.jpg` }}>
-                  <span className="name">{site.name_en}</span>
-                  <span className="category">{site.category}</span>
-                  <span className="distance">{Math.round(site.distance / 1000)} km</span>
-                </a></li>
-              )}
-            </ul></div>
-        }
-
-        <h2>Timing</h2>
-        <ul>
-          <li>
-            Secure WebSocket (wss://) to co-located proxy and DB: {' '}
-            {gmwss_1_data?.duration ?? '...'} ms, {' '}
-            {gmwss_2_data?.duration ?? '...'} ms, {' '}
-            {gmwss_3_data?.duration ?? '...'} ms, {' '}
-            {gmwss_4_data?.duration ?? '...'} ms, {' '}
-            {gmwss_5_data?.duration ?? '...'} ms {' '}
-          </li>
-        </ul>
         <p>
           Heritage site data copyright &copy; 1992 – {new Date().getFullYear()} {' '}
           <a href="https://whc.unesco.org">UNESCO/World Heritage Centre</a>. All rights reserved.
